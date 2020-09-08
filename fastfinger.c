d4c59373e2bb547f8f348d1ea23394f8d7d5300b
Fonsy <ciabadiale@gmail.com>
2020-09-08 15:51:39 +0200

    the word to type is now highlighted

--- fastfinger.c
+++ fastfinger.c
@@ -3,10 +3,15 @@
 #include <stdlib.h>
 
 #define N_WORDS 8
+#define MAX_WORDS 255
 #define WORD_LENGHT 8
+
 #define KEY_ESC 27
+
+/* Color stuff */
 #define RIGHT_WORD 1
 #define WRONG_WORD 2
+#define SELECT_WORD 3
 
 int typing_word_correctly(char *user, char *word_to_type);
 
@@ -29,6 +34,7 @@ int main()
 	start_color();
 	init_pair(RIGHT_WORD, COLOR_GREEN, COLOR_BLACK);
 	init_pair(WRONG_WORD, COLOR_RED, COLOR_BLACK);
+	init_pair(SELECT_WORD, COLOR_BLACK, COLOR_WHITE);
 
 	/* Print the word to type in the center of the screen */
 	size_t word_lenght = 0;
@@ -53,7 +59,12 @@ int main()
 	word_to_type = (char*)malloc(WORD_LENGHT*sizeof(char));
 
 	strcpy(word_to_type, words[index_word_to_type]);
+
 	word_lenght = 0;
+
+	attron(COLOR_PAIR(SELECT_WORD));
+	mvprintw(5, x_offset, word_to_type);
+	attroff(COLOR_PAIR(SELECT_WORD));
 
 	move(6, (COLS/2) - 5);
 	while((ch = getch()) != KEY_ESC)
@@ -68,36 +79,42 @@ int main()
 				n_ch--;
 				break;
 			case ' ':
+				// end the string
 				*user_word = '\0';
 				user_word -= n_ch;
 				n_ch = 0;
 
+				// update the word that must be typed
+				if (index_word_to_type < N_WORDS - 1)
+					index_word_to_type++;
+				else
+					index_word_to_type = 0, word_lenght = 0;
+
+				// check if the word typed is correct
 				if(typing_word_correctly(user_word, word_to_type))
 				{
 					correct_typed_words++;
 					attron(COLOR_PAIR(RIGHT_WORD));
-					mvprintw(5, x_offset + word_lenght, word_to_type);
-					word_lenght += strlen(word_to_type) + 1;
 
-				} else {
+				} else 
 					attron(COLOR_PAIR(WRONG_WORD));
-					mvprintw(5, x_offset + word_lenght, word_to_type);
-					word_lenght += strlen(word_to_type) + 1;
-				}
 
-				if (index_word_to_type < N_WORDS - 1)
-					index_word_to_type++;
-				else
-					index_word_to_type, word_lenght = 0;
+				// mark as correct or wrong the current typed word
+				mvprintw(5, x_offset + word_lenght, word_to_type);
+				attroff(COLOR_PAIR(WRONG_WORD));
+				attroff(COLOR_PAIR(RIGHT_WORD));
+
+				word_lenght += strlen(word_to_type) + 1;
 				strcpy(word_to_type, words[index_word_to_type]);
 
-				/* DEBUG */
-				/* move(0,0); */
-				/* clrtoeol(); */
-				/* printw("word to type: %s", words[index_word_to_type]); */
+				// select next word to be typed
+				attron(COLOR_PAIR(SELECT_WORD));
+				mvprintw(5, x_offset + word_lenght, word_to_type);
 
+				// reset the cursor
 				move(6, (COLS/2) - 5);
 				clrtoeol();
+
 				break;
 			case '\n':
 				break;
