fda3c90ce2c12e0dc6a3d0e5f48180b191a9ee0c
Fonsy <ciabadiale@gmail.com>
2020-09-08 15:14:50 +0200

    added color feedback

--- fastfinger.c
+++ fastfinger.c
@@ -3,10 +3,14 @@
 #include <stdlib.h>
 
 #define N_WORDS 8
+#define WORD_LENGHT 8
 #define KEY_ESC 27
+#define RIGHT_WORD 1
+#define WRONG_WORD 2
 
-int
-main()
+int typing_word_correctly(char *user, char *word_to_type);
+
+int main()
 {
 	int ch;
 	char *words[N_WORDS] = {"ciao", "prova", "test", "luce", "astronomo", "fiore", "pianeta", "inventore"};
@@ -16,6 +20,15 @@ main()
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
+
+	if(has_colors() == FALSE)
+	{	endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+	start_color();
+	init_pair(RIGHT_WORD, COLOR_GREEN, COLOR_BLACK);
+	init_pair(WRONG_WORD, COLOR_RED, COLOR_BLACK);
 
 	/* Print the word to type in the center of the screen */
 	size_t word_lenght = 0;
@@ -32,11 +45,17 @@ main()
 	}
 
 	/* Prepare to user input */
-	move(6, (COLS/2) - 5);
-	int cursor_x, cursor_y, n_ch = 0;
-	char *user_word;
-	user_word = (char*)malloc(32*sizeof(char));
+	int cursor_x, cursor_y, n_ch = 0, index_word_to_type = 0;
+	int correct_typed_words = 0;
 
+	char *user_word, *word_to_type;
+	user_word = (char*)malloc(WORD_LENGHT*sizeof(char));
+	word_to_type = (char*)malloc(WORD_LENGHT*sizeof(char));
+
+	strcpy(word_to_type, words[index_word_to_type]);
+	word_lenght = 0;
+
+	move(6, (COLS/2) - 5);
 	while((ch = getch()) != KEY_ESC)
 	{
 		getyx(stdscr, cursor_y, cursor_x);
@@ -53,17 +72,46 @@ main()
 				user_word -= n_ch;
 				n_ch = 0;
 
-				move(0, 0);
-				clrtoeol();
-				printw("The word is %s", user_word);
+				if(typing_word_correctly(user_word, word_to_type))
+				{
+					correct_typed_words++;
+					attron(COLOR_PAIR(RIGHT_WORD));
+					mvprintw(5, x_offset + word_lenght, word_to_type);
+					word_lenght += strlen(word_to_type) + 1;
+
+				} else {
+					attron(COLOR_PAIR(WRONG_WORD));
+					mvprintw(5, x_offset + word_lenght, word_to_type);
+					word_lenght += strlen(word_to_type) + 1;
+				}
+
+				if (index_word_to_type < N_WORDS - 1)
+					index_word_to_type++;
+				else
+					index_word_to_type, word_lenght = 0;
+				strcpy(word_to_type, words[index_word_to_type]);
+
+				/* DEBUG */
+				/* move(0,0); */
+				/* clrtoeol(); */
+				/* printw("word to type: %s", words[index_word_to_type]); */
 
 				move(6, (COLS/2) - 5);
 				clrtoeol();
 				break;
+			case '\n':
+				break;
 			default:
-				addch(ch);
 				*(user_word++) = ch;
+				*user_word = '\0';
 				n_ch++;
+
+				if( typing_word_correctly(user_word - n_ch, word_to_type) )
+					attron(COLOR_PAIR(RIGHT_WORD));
+				else
+					attron(COLOR_PAIR(WRONG_WORD));
+
+				addch(ch);
 				break;
 		}
 	}
@@ -73,3 +121,10 @@ main()
 	return 0;
 }
 
+int typing_word_correctly(char *user, char *word_to_type){
+	for( ; *user != '\0' ; user++, word_to_type++ ){
+		if (*user != *word_to_type)
+			return 0;
+	}
+	return 1;
+}
