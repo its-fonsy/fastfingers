ee60ef84ed8d3efba96d5cbe692127e6da671b23
Fonsy <ciabadiale@gmail.com>
2020-09-13 17:02:54 +0200

    clear code

--- src/util.h
+++ src/util.h
@@ -7,7 +7,7 @@
 int typing_word_correctly(char *user, char *word_to_type);
 int word_typed_right(char *user, char *word_to_type);
 int string_len(char *s);
-
 int shuffle(char *array[], int len);
 int feed_words_into_array(char* filename, char* array[]);
+
 #endif
